3e52409fd3d84e3988f438e3c708113d6a24c291
rickdean <rickdean@outlook.com>
2016-10-23 07:22:48 -0400

    Commit All Study Files
    Commit completed study files

---
+++ accessing-array-values.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let food = ['apple', 'pizza', 'pear'];
+
+console.log(food[1]);
---
+++ for-loop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let total = 0;
+let limit = 10;
+
+for (let i = 0; i < limit; i++) {
+  total += i;
+}
+console.log(total);
